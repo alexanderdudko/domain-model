3f70c852794ba0683de0b634e3845c8801401710
Alexander <aleksandrsdudko@gmail.com>
2020-03-27 18:44:26 +0200

    Initial solution and code created

---
+++ DomainModel/DomainModel/Model.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DomainModel
+{
+    public class Model : Entity { }
+    public class Entity : Collection<Property> { }
+    public class Collection<T> : List<T> { }
+    public class Property { }
+
+
+
+    public class HospitalModel : Model
+    {
+        public Collection<PersonEntity> Doctors { get; set; }
+        public Collection<PersonEntity> Patients { get; set; }
+        public Collection<VisitRecordEntity> Visits { get; set; }
+    }
+
+    public class PersonEntity : Entity { }
+    public class VisitRecordEntity : Entity { }
+}
